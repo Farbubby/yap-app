d161fd818a07912c84b90c7043de0c7d8d407193
farbubby <farhan.mahbub@ucf.edu>
2024-05-10 19:23:13 -0400

    Added text input for alias/nicknames and use react hook to get the user inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [alias, setAlias] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
       <div className="flex flex-col justify-center items-center h-screen gap-14 bg-gray-950">
@@ -12,16 +18,24 @@ export default function Home() {
             Register an account to start yapping
           </div>
           <form className="flex flex-col gap-3">
+            <label className="text-xs">Alias</label>
+            <input
+              id="alias"
+              type="text"
+              className="rounded-lg bg-gray-950 border-gray-700 border py-1 px-2 hover:bg-gray-900"
+              onChange={(e) => setAlias(e.target.value)}></input>
             <label className="text-xs">Username</label>
             <input
               id="username"
               type="text"
-              className="rounded-lg bg-gray-950 border-gray-700 border py-1 px-2 hover:bg-gray-900"></input>
+              className="rounded-lg bg-gray-950 border-gray-700 border py-1 px-2 hover:bg-gray-900"
+              onChange={(e) => setUsername(e.target.value)}></input>
             <label className="text-xs">Password</label>
             <input
               id="password"
               type="password"
-              className="rounded-lg bg-gray-950 border-gray-700 border py-1 px-2 hover:bg-gray-900"></input>
+              className="rounded-lg bg-gray-950 border-gray-700 border py-1 px-2 hover:bg-gray-900"
+              onChange={(e) => setPassword(e.target.value)}></input>
             <input
               type="submit"
               value="Register"
