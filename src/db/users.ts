29bc4e8b7e417c3ce518767e04a5c7e2cee539b0
farbubby <farhan.mahbub@ucf.edu>
2024-05-11 20:52:16 -0400

    Added the rest of the CRUD operations for users

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,5 +1,6 @@
-import { prisma } from "./client";
+import { prisma } from "./prisma";
 
+// CREATE
 export const createUser = async (
   alias: string,
   username: string,
@@ -10,6 +11,46 @@ export const createUser = async (
       alias,
       username,
       password,
+    },
+  });
+};
+
+// GET
+export const getUser = async (username: string) => {
+  return prisma.user.findUnique({
+    where: {
+      username,
+    },
+  });
+};
+
+export const getUsers = async () => {
+  return prisma.user.findMany();
+};
+
+// UPDATE
+export const updateUser = async (
+  username: string,
+  alias: string,
+  password: string
+) => {
+  return prisma.user.update({
+    where: {
+      username,
+    },
+    data: {
+      alias,
+      username,
+      password,
+    },
+  });
+};
+
+// DELETE
+export const deleteUser = async (username: string) => {
+  return prisma.user.delete({
+    where: {
+      username,
     },
   });
 };
