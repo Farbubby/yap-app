c6003d9a50df1d7ff45aa57f664a990acd111c94
farbubby <farhan.mahbub@ucf.edu>
2024-05-12 17:12:52 -0400

    Added input validation and error handling for sign ups

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,4 +1,4 @@
-import { createUser } from "@/db/users";
+import { createUser, getUser } from "@/db/users";
 
 export async function POST(req: Request) {
   const { alias, username, password } = (await req.json()) as {
@@ -7,11 +7,27 @@ export async function POST(req: Request) {
     password: string;
   };
 
+  if (!alias || !username || !password) {
+    return new Response("Missing alias, username, or password", {
+      status: 400,
+    });
+  }
+
+  if (password.length < 8) {
+    return new Response("Password must be at least 8 characters long", {
+      status: 400,
+    });
+  }
+
+  if (await getUser(username)) {
+    return new Response("Username already exists", { status: 409 });
+  }
+
   const user = await createUser(alias, username, password);
 
   if (!user) {
-    return Response.error();
-  } else {
-    return Response.json({ user });
+    return new Response("Failed to create user", { status: 500 });
   }
+
+  return new Response(JSON.stringify(user), { status: 201 });
 }
