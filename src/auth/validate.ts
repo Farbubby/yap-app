b85c8c1c7af4284e94b392db240e3faa7760d6b3
farbubby <farhan.mahbub@ucf.edu>
2024-05-20 14:22:22 -0400

    Eliminated the need for a REST endpoint for user authorization

---
+++ src/auth/validate.ts
@@ -0,0 +1,18 @@
+import { lucia } from "@/auth/lucia";
+import { cookies } from "next/headers";
+
+export const validateUser = async function () {
+  const sessionId = cookies().get(lucia.sessionCookieName)?.value || "";
+
+  if (!sessionId) {
+    return { user: null, session: null };
+  }
+
+  const { user, session } = await lucia.validateSession(sessionId);
+
+  if (!user || !session) {
+    return { user: null, session: null };
+  }
+
+  return { user, session };
+};
