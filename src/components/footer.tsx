b64ed92a3f98184bf2d95cc46a0e094f9c91206e
farbubby <farhan.mahbub@ucf.edu>
2024-06-22 18:31:56 -0400

    Small fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ import CreatePostButton from "./create-post-button";
 export default function Footer() {
   return (
     <>
-      <div className="flex flex-row justify-end items-center py-8 px-10 fixed w-full bottom-0">
+      <div className="flex flex-row justify-end items-center py-8 px-10 fixed w-ful right-0 bottom-0">
         <CreatePostButton />
       </div>
     </>
